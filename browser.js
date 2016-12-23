dad2d4d853e02c76b119e2cb2d6b479a4ff21776
Adrien Gibert <agibert@atl.yearup.com>
2016-12-23 18:09:27 -0500

    Test 2

--- browser.js
+++ browser.js
@@ -13,13 +13,14 @@
     s.search(obj.Song + " " + obj.Artist, ['track'], {limit: 1})
 			.then(function(data){
 				var songInfo = data.tracks.items[0]
+				var album = songInfo.album.name
+				var albumImg = songInfo.album.images[1]
+				var artist = songInfo.artists[0].name
+				var votes = obj.NumVotes
+				var preview = songInfo.preview_url
+				var track = songInfo.name
 			})
-	var album = songInfo.album.name
-	var albumImg = songInfo.album.images[1]
-	var artist = songInfo.artists[0].name
-	var votes = obj.NumVotes
-	var preview = songInfo.preview_url
-	var track = songInfo.name
+	
 
 	$('body').append("<div><audio src="+preview+ " controls></audio><img src="+albumImg+"><ul><li>" + track + "</li><li>" + artist+ "</li><li>"+ album + "</li><li>"+ votes + "</ul></div>")
 }
